a1724c659455a428990168860755a98370726f98
ddocria <149016218+AdemirNetoDev@users.noreply.github.com>
2023-12-06 16:44:44 -0300

    att

---
+++ aulasjs/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h!`)
+if (vel > 60) { //condicao simples!
+    console.log(`Voce ultrapassou o limite de velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de seguranca!`)
---
+++ aulasjs/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Estados Unidos'
+console.log(`Vivendo em ${país}`)
+if (país != 'Brazil') {
+    console.log(`Brasileiro!`)
+} else {
+    console.log(`Estrangeiro`)
+}
