e0f40b0b14c5cc1ab14fff2be5552207fe22654f
ddocria <149016218+AdemirNetoDev@users.noreply.github.com>
2023-12-11 16:56:48 -0300

    att

--- aulasjs/aula12ex/ex014/script.js
+++ aulasjs/aula12ex/ex014/script.js
@@ -1,1 +1,24 @@
-alert('Olá!')
+
+
+function carregar() {
+
+    var msg = window.document.getElementById('msg')
+    var foto = window.document.getElementById('foto')
+    var data = new Date()
+
+    //var hora = data.getHours()
+
+    var hora = 19
+    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.style.textAlign = 'center' 
+
+    if (hora >= 0 && hora < 12) {
+        document.body.style.background = '#e2cd9f'
+
+    } else if (hora >= 12 && hora <= 18) {
+        document.body.style.background = '#b9846f'
+
+    } else {
+        document.body.style.background = '#515154'
+    }
+}
