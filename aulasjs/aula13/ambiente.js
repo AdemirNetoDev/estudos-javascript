7673b7056f291916e7f95937fefa1eca67ea3c78
ddocria <149016218+AdemirNetoDev@users.noreply.github.com>
2023-12-13 16:28:18 -0300

    att

---
+++ aulasjs/aula13/ambiente.js
@@ -0,0 +1,26 @@
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+
+
+/*
+ESTRUTURAS DE REPETICAO COM TESTE LÓGICO NO INÍCIO
+var c = 1
+while (c <= 10) {
+    console.log('Passo ' + c)
+    c++ // c = c + 1
+}
+*/
+
+
+//ESTRUTURAS DE REPETICAO COM TESTE LÓGICO NO FINAL
+var c = 1
+do {
+    console.log('Passo '+ c)
+    c++
+} while (c <= 6)
---
+++ aulasjs/aula14/ambiente.js
@@ -0,0 +1,20 @@
+/*
+var c = 1 
+while (c <=10) {
+    console.log('Passo '+ 1)
+    c++
+}
+*/
+
+
+/*
+for (var c = 1; c <=10; c++){
+    console.log('Passo '+ c)
+}
+*/
+
+console.log('Vai comecar...')
+for(var c = 1;c <=4;c++) {
+    console.log(c)
+}
+console.log('FIM!')
