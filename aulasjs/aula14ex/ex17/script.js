290d7ca0282a526c592d8b440ccb92b678dec874
ddocria <149016218+AdemirNetoDev@users.noreply.github.com>
2023-12-18 16:51:32 -0300

    att

--- aulasjs/aula14ex/ex17/script.js
+++ aulasjs/aula14ex/ex17/script.js
@@ -1,8 +1,8 @@
 function tabuada() {
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
-    if (num.value.length == 0) {
-    window.alert('Por favor, digite um número!')
+    if (num.value == 0) {
+        window.alert('Por favor, digite um número!')
     } else {
         let n = Number(num.value)
         let c = 1
