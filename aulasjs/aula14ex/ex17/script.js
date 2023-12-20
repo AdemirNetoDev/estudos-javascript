1c673259dc3b22ceece22ab9c396a4e5f2ee980b
ddocria <149016218+AdemirNetoDev@users.noreply.github.com>
2023-12-20 19:39:17 -0300

    att

--- aulasjs/aula14ex/ex17/script.js
+++ aulasjs/aula14ex/ex17/script.js
@@ -1,15 +1,12 @@
-function tabuada(){
-    let num = document.getElementById('txtn')
-    let tab = document.getElementById('seltab')
-    
-    if (num.value < 1 || num.value > 12) {
-        tab.innerHTML = ('Insira um número no intervalo de 1 a 12!')
-    } else {
+var num = document.getElementById('txtn')
+var res = document.getElementById('seltab')
 
-        for(c = 1; c <= 10; c++){
-            var calc = String(c) + ' x ' + num.value + ' = ' + c * Number(num.value);
-            var resultado = document.createElement('p')
-            resultado.innerText = calc
-            tabuada.appenChild(resultado)
+function tabuada() {
+    if (Number(num.value) < 1 || Number(num.value > 10)){
+        window.alert('Digite um número entre 1 e 10.')
+    } else {
+        for(c = 1; c >= 1 && c <= 10; c++) {
+            document.write(c)
+        }
     }
-                            }
+}
